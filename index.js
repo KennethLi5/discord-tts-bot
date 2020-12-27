710fd4c4c6156d4dc0b86fea2d4737227c2038b1
brandon wetzel <brandom.m.wetzel@gmail.com>
2020-12-27 00:34:26 -0500

    using polly

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const Discord = require('discord.js');
 const Stream = require('stream');
-const https = require('https');
-const googleTTS = require('google-tts-api');
+const AWS = require('aws-sdk');
 
 const {
 	command,
-	token,
+    token,
+    key,
+    secretkey,
 } = require('./config.json');
 
 const client = new Discord.Client();
 const queue = new Map();
+const Polly = new AWS.Polly({
+    signatureVersion: 'v4',
+    region: 'us-east-1',
+    accessKeyId: key,
+    secretAccessKey: secretkey
+})
+
+client.login(token);
 
 client.once('ready', () => {
     console.log('Ready!');
@@ -21,100 +30,72 @@ client.once('disconnect', () => {
     console.log('Disconnect!');
 });
 
-function getVoiceStream(text, lang="en-GB", speed=1){
-    const stream=new Stream.PassThrough();
-    downloadFromInfoCallback(text,lang,speed,stream);
-    return stream;
-}
-
-function downloadFromInfoCallback(text, lang, speed, stream){
-    console.log(text);
-    const url = googleTTS.getAudioUrl(text, {lang: lang})
-    
-    console.log(url);
-
-    const request = https.get(url, function(response,err) {
-        response.pipe(stream);
-    });
-
-}
-
-function play(serverId, serverQ){
-    if(serverQ.messages.length == 0){
-        serverQ.voiceChannel.leave();
-        queue.delete(serverId);
-        return;
-    }
-    const broadcast = serverQ.broadcast.play(
-        getVoiceStream(serverQ.messages.shift())
-    );
-
-    const dispatcher = serverQ.connection
-    .play(broadcast)
-    .on("error", error => console.log(error))
-    .on("finish", () => play(serverId, serverQ))
-
-};
 
 client.on('message', async message => {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(command)) return;
+    if (message.author.bot) 
+        return;
+
+    if (!message.content.startsWith(command))
+        return;
 
     const args = message.content.slice(command.length).trim();
-    const serverId = message.guild.id;
-    let serverQ = queue.get(serverId);
 
     try{ 
         const broadcast = client.voice.createBroadcast();
         var channelId=message.member.voice.channelID;
         var channel=client.channels.cache.get(channelId);
+
+        var params = {
+            OutputFormat: 'mp3',
+            Text: args,
+            VoiceId: 'Brian',            
+        }
+
         channel.join().then(connection => {
-            broadcast.play(getVoiceStream(args));
-            const dispatcher=connection.play(broadcast);
+            console.log(args);
+            Polly.synthesizeSpeech(params, function(err, data) {
+                var bufferStream = new Stream.PassThrough()
+                bufferStream.end(data.AudioStream)                
+                broadcast.play(bufferStream)
+                const dispatcher=connection.play(broadcast);
+            })
         });
     }catch(error){
         console.error(error);
     }
-
-    
-
-    // try {
-    //     const vc = message.member.voice.channel;
-    //     // console.log(channelId, message.guild.id);
-    //     if(!vc) {message.reply('you must be in a voice channel'); return;}
-    //     if(!serverQ) {
-    //         let newQ = {
-    //             textChannel: message.channel, 
-    //             voiceChannel: vc,
-    //             connection: null,
-    //             broadcast: null,
-    //             messages: [],
-    //             volume: 5, 
-    //             playing: true
-    //         };
-    //         queue.set(serverId, newQ);
-    //         newQ.connection = await vc.join();
-    //         newQ.broadcast = client.voice.createBroadcast();
-    //         newQ.messages.push(args);
-            
-    //         play(serverId, newQ);
-        
-        
-    //     }else{
-    //         serverQ.messages.push(args);
-    //         console.log(serverQ.messages);
-    //         return;
-    //     };
-        
-        
-        
-
-        
-    // }catch(error){
-    //     console.error(error);
-    //     message.reply('error executing command. sorry about that king. ')
-    // }
-
 })
 
-client.login(token);
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    let newUserChannel = newMember.channelID;
+    let oldUserChannel = oldMember.channelID;
+    var channelId= '761963539950469121';
+    var channel=client.channels.cache.get(channelId);
+    const broadcast = client.voice.createBroadcast();
+
+
+
+    channel.join().then(connection => { 
+
+        if (newUserChannel != oldUserChannel) {
+            if (newUserChannel == channelId) {
+
+                if (newMember.member  == '154315825229201409') {
+                    console.log("master");
+                } 
+            
+                var params = {
+                    OutputFormat: 'mp3',
+                    Text: 'This is Chad 1 to Chad 3. Can we have some YEP Cock please?',
+                    VoiceId: 'Brian',        
+                }
+
+                Polly.synthesizeSpeech(params, function(err, data) {            
+                    var bufferStream = new Stream.PassThrough()
+                    bufferStream.end(data.AudioStream)                
+                    broadcast.play(bufferStream)
+                    const dispatcher=connection.play(broadcast);
+                })                
+            } 
+        }
+    })
+})
