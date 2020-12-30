0a76a8fab6cc27500a3810fcf96c2ddf6c0969bc
Kenenth Li <kenneth.li.015@gmail.com>
2020-12-30 18:54:50 -0500

    Added join and leave commands. Added soundcloud and youtube download functions. Updated TTS and on join message

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const Discord = require('discord.js');
 const Stream = require('stream');
 const AWS = require('aws-sdk');
+const ytdl = require('ytdl-core');
+const scdl = require('soundcloud-downloader').default;
 
 const {
-	command,
-    token,
-    key,
-    secretkey,
+	prefix,
+    discToken,
+    awsKey,
+    awsSecretKey,
+    awsVoice,
+    scClientID,
+    scLink
 } = require('./config.json');
 
 const client = new Discord.Client();
@@ -14,11 +19,13 @@ const queue = new Map();
 const Polly = new AWS.Polly({
     signatureVersion: 'v4',
     region: 'us-east-1',
-    accessKeyId: key,
-    secretAccessKey: secretkey
+    accessKeyId: awsKey,
+    secretAccessKey: awsSecretKey
 })
 
-client.login(token);
+var botID = 1; //leaving discord call sets user voice ID to null. setting botID to null will cause error when leaving voice channel because of newUserChannel == botID and channel.join
+
+client.login(discToken);
 
 client.once('ready', () => {
     console.log('Ready!');
@@ -30,75 +37,105 @@ client.once('disconnect', () => {
     console.log('Disconnect!');
 });
 
-function synthesizeSpeech(params, con, broadcast) {
-    Polly.synthesizeSpeech(params, function(err, data) {
+function synthesizeSpeech(params, connection, broadcast) { // retrieve Amazon Polly audio 
+    Polly.synthesizeSpeech(params, function(err, data) {        
         var bufferStream = new Stream.PassThrough()
         bufferStream.end(data.AudioStream)                
         broadcast.play(bufferStream)
-        const dispatcher=con.play(broadcast);
+        connection.play(broadcast);
     })
 }
 
-client.on('message', async message => {
-    if (message.author.bot) 
+function youtubeAudio(link, connection) { //retrieve YouTube audio 
+    var bufferStream = ytdl(link, {
+        filter: 'audioonly'
+    })
+
+    connection.play(bufferStream);
+}
+
+client.on('message', async message => {    // WIP for direct bot dm commands
+    if (message.channel.type == "dm") {
         return;
-
-    if (!message.content.startsWith(command))
-        return;
-
-    const args = message.content.slice(command.length).trim();
-
-    try{ 
-        const broadcast = client.voice.createBroadcast();
-        var channelId=message.member.voice.channelID;
-        var channel=client.channels.cache.get(channelId);
-
-        var params = {
-            OutputFormat: 'mp3',
-            Text: args,
-            VoiceId: 'Brian',            
-        }
-        console.log(channelId);
-
-        channel.join().then(connection => {
-            synthesizeSpeech(params, connection, broadcast)
-        });
-    }catch(error){
-        console.error(error);
     }
 })
 
-client.on('voiceStateUpdate', (oldMember, newMember) => {
-    let newUserChannel = newMember.channelID;
-    let oldUserChannel = oldMember.channelID;
-    var channelId= '761963539950469121';
-    var channel=client.channels.cache.get(channelId);
-    const broadcast = client.voice.createBroadcast();
-
-
-
-    channel.join().then(connection => { 
-
-        if (newUserChannel != oldUserChannel) {
-            if (newUserChannel == channelId) {
-
-                if (newMember.member  == '154315825229201409') {
-                    console.log("master");
-                } 
-            
-                var params = {
-                    OutputFormat: 'mp3',
-                    Text: 'This is Chad 1 to Chad 3. Can we have some YEP Cock please?',
-                    VoiceId: 'Brian',        
-                }
-
-                Polly.synthesizeSpeech(params, function(err, data) {            
-                    var bufferStream = new Stream.PassThrough()
-                    bufferStream.end(data.AudioStream)                
-                    broadcast.play(bufferStream)
-                    const dispatcher=connection.play(broadcast);
-                })                
-            } 
+client.on('message', async message => { // join and leave commands
+    if (message.channel.type == "text") {                  
+        if (message.content === prefix + 'join') {
+            try{ 
+                var channelID=message.member.voice.channelID;            
+                var channel=client.channels.cache.get(channelID);
+                channel.join();
+                botID = channelID; // update botID for playing sound. to-do: check if bot is in voice channel without global botID
+                console.log("Joined voice channel: " + message.member.voice.channel.name);
+                return;
+            }catch(error){
+                console.error(error);
+            }
         }
-    })
+        if (message.content === prefix + 'leave') {
+            try{ 
+                if (message.guild.voice.connection) {
+                    message.guild.me.voice.channel.leave();
+                    console.log("Left voice channel: " + message.guild.me.voice.channel.name);                            
+                    return;
+                }
+            }catch(error){
+                console.error(error);
+            }
+        }
+    }
+})
+
+client.on('message', async message => { // Text to Speech
+    if (message.channel.type == "text") {        
+        if (message.content === prefix + 'tts') { 
+            await message.reply('please enter a message.');
+            return;
+        }    
+        if (!message.content.startsWith(prefix + 'tts '))       
+            return;            
+
+        const msg = message.content.slice((prefix+'tts').length).trim();
+
+        try{ 
+            const broadcast = client.voice.createBroadcast();
+            var channelID=message.member.voice.channelID;
+            var channel=client.channels.cache.get(channelID);
+            
+            var params = {
+                OutputFormat: 'mp3',
+                Text: msg,
+                VoiceId: awsVoice,            
+            }
+            
+            channel.join().then(connection => {                
+                console.log("TTS in voice channel: " + message.member.voice.channel.name + ". Message: " + msg);
+                synthesizeSpeech(params, connection, broadcast)                
+            });
+        }catch(error){
+            console.error(error);
+        }
+    }
+})
+
+client.on('voiceStateUpdate', (oldMember, newMember) => { // Play sound when someone joins 
+    let newUserChannel = newMember.channelID;
+    let oldUserChannel = oldMember.channelID;    
+    var channel = client.channels.cache.get(botID);
+            
+        if (newUserChannel != oldUserChannel) {
+            if (newUserChannel == botID) {      
+                try{  
+                    channel.join().then(connection => {
+                        scdl.download(scLink, scClientID).then(stream => {
+                            connection.play(stream);
+                        })       
+                    })
+                }catch(error){
+                    console.error(error);
+                }
+            }
+        }  
 })
